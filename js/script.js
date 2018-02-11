bdde5c3c2f746b231b0c63a8cdb3c7a1869e5d99
KedamaOvO <moe@mao-yu.net>
2018-02-11 17:14:40 +0800

    0.0.1.1

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ var html_pp=document.querySelector("#pp-lable");
 
 
 setInterval(function(){
-	$.get("http://localhost:10800/api/pp",function(pp)
+	$.get("http://localhost:10800/api/pp_formated",function(pp)
 	{
-		html_pp.textContent=pp.RealTimePP.toFixed(2)+"pp";
+		html_pp.textContent=pp.Formated;
 	});
 	
-	$.get("http://localhost:10800/api/hit_count",function(hit_count)
+	$.get("http://localhost:10800/api/hit_count_formated",function(hit_count)
 	{
-		html_hit_count.textContent=hit_count.Count100+"x100 "+hit_count.Count50+"x50 "+hit_count.CountMiss+"xMiss";
+		html_hit_count.textContent=hit_count.Formated;
 }	);
 },100);
