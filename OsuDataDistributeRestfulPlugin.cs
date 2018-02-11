2d383d038ac3d94a8f9219379336e933f0cf0a5c
KedamaOvO <moe@mao-yu.net>
2018-02-11 16:13:10 +0800

    firsr commit

---
+++ OsuDataDistributeRestfulPlugin.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using RealTimePPDisplayer;
+using Sync.Command;
+using Sync.Plugins;
+using Sync.Tools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuDataDistribute_Restful
+{
+    [SyncPluginDependency("8eb9e8e0-7bca-4a96-93f7-6408e76898a9", Version = "^1.2.3", Require = true)]
+    [SyncPluginID("4b045b1c-7ab2-41a7-9f80-7e79c0d7768a", VERSION)]
+    public class OsuDataDistributeRestfulPlugin : Plugin
+    {
+        public const string PLUGIN_NAME = "OsuDataDistribute-Restful";
+        public const string PLUGIN_AUTHOR = "KedamavOvO";
+        public const string VERSION = "0.0.1";
+
+        private HttpListener m_httpd=new HttpListener();
+        private Dictionary<string, Func<object>> m_url_dict = new Dictionary<string, Func<object>>();
+
+        private RestfulDisplayer m_displayer;
+
+        public OsuDataDistributeRestfulPlugin() : base(PLUGIN_NAME, PLUGIN_AUTHOR)
+        {
+
+        }
+
+        private void Initialize()
+        {
+            var plugin=getHoster().EnumPluings().Where(p => p.Name == "RealTimePPDisplayer").FirstOrDefault();
+            if (plugin is RealTimePPDisplayerPlugin rtppd)
+            {
+                rtppd.RegisterDisplayer("restful", (id) => m_displayer = new RestfulDisplayer());
+                RegisterResource("/api/is_play", () => m_displayer.IsPlay);
+                RegisterResource("/api/pp", () => m_displayer.PPTuple);
+                RegisterResource("/api/hit_count", () => m_displayer.HitCountTuple);
+            }
+            else
+                IO.CurrentIO.WriteColor($"Not Founf RealTimePPDisplayer", ConsoleColor.Red);
+        }
+
+        private void RegisterResource(string uri,Func<object> c)
+        {
+            m_url_dict.Add(uri, c);
+        }
+
+        public override async void OnEnable()
+        {
+            Initialize();
+            m_httpd.Prefixes.Add(@"http://localhost:10800/");
+            m_httpd.Start();
+            while (true)
+            {
+                var ctx = await m_httpd.GetContextAsync();
+                var request = ctx.Request;
+                var response = ctx.Response;
+
+                response.ContentEncoding = Encoding.UTF8;
+                response.ContentType = "text/json; charset=UTF-8";
+
+                if(request.HttpMethod=="GET")
+                {
+                    if (m_url_dict.TryGetValue(request.Url.AbsolutePath, out var func))
+                    {
+                        var json=JsonConvert.SerializeObject(func());
+                        ctx.Response.StatusCode = 200;
+
+                        using (var sw = new StreamWriter(response.OutputStream))
+                            sw.Write(json);
+                    }
+                    else
+                    {
+                        ctx.Response.StatusCode = 404;
+                        using (var sw = new StreamWriter(response.OutputStream))
+                            sw.Write("{\"code\":404}");
+                    }
+                }
+                else
+                {
+                    response.StatusCode = 403;
+                    using (var sw = new StreamWriter(response.OutputStream))
+                        sw.Write("{\"code\":403}");
+                }
+                response.OutputStream.Close();
+            }
+        }
+    }
+}
---
+++ RestfulDisplayer.cs
@@ -0,0 +1,34 @@
+﻿using RealTimePPDisplayer.Displayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuDataDistribute_Restful
+{
+    class RestfulDisplayer : DisplayerBase
+    {
+        public bool IsPlay { get; private set; } = false;
+        public HitCountTuple HitCountTuple { get; private set; }
+        public PPTuple PPTuple { get; private set; }
+
+        public override void Clear()
+        {
+            IsPlay = false;
+            HitCountTuple = new HitCountTuple();
+            PPTuple = PPTuple.Empty;
+        }
+
+        public override void OnUpdateHitCount(HitCountTuple tuple)
+        {
+            HitCountTuple = tuple;
+        }
+
+        public override void OnUpdatePP(PPTuple tuple)
+        {
+            IsPlay = true;
+            PPTuple = tuple;
+        }
+    }
+}
