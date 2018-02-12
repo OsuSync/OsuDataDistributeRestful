8f0db0ed7ca4bdc44f80425fa87e0e1dc9650ff7
KedamaOvO <moe@mao-yu.net>
2018-02-12 21:11:25 +0800

    update html

--- html/js/script.js
+++ html/js/script.js
@@ -1,15 +1,23 @@
 var html_hit_count=document.querySelector("#hit-count-lable");;
 var html_pp=document.querySelector("#pp-lable");
 
+var _url=new URL(window.location.href);
+var host=_url.searchParams.get('host');
+var port=_url.searchParams.get('port');
+
+if(host==null)host="localhost";
+if(port==null)port="10800";
 
 
 setInterval(function(){
-	$.get("http://localhost:10800/api/pp_formated",function(pp)
+	var web="http://"+host+":"+10800;
+	
+	$.get(web+"/api/pp_formated",function(pp)
 	{
 		html_pp.textContent=pp.Formated;
 	});
 	
-	$.get("http://localhost:10800/api/hit_count_formated",function(hit_count)
+	$.get(web+"/api/hit_count_formated",function(hit_count)
 	{
 		html_hit_count.textContent=hit_count.Formated;
 }	);
