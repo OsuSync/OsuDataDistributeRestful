244347564362786e2092adee5a0fa1e36710ae3a
KedamaOvO <moe@mao-yu.net>
2018-02-28 21:16:59 +0800

    add i18n

---
+++ DefaultLanguage.cs
@@ -0,0 +1,17 @@
+﻿using Sync.Tools;
+using Sync.Tools.ConfigGUI;
+
+namespace OsuDataDistributeRestful
+{
+    public class DefaultLanguage : I18nProvider
+    {
+        [ConfigI18n]
+        public static LanguageElement AllowLAN = "Allow LAN";
+
+        [ConfigI18n]
+        public static LanguageElement EnableFileHttpServer = "Enable file HTTP server";
+
+        [ConfigI18n]
+        public static LanguageElement FileServerRootPath = "File server root path";
+    }
+}
