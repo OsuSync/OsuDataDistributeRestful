9e2dbbeb1a88940dd02e43138c59e5057348a1dc
KedamaOvO <moe@mao-yu.net>
2018-08-13 16:26:46 +0800

    update open_firewall.bat

--- open_firewall.bat
+++ open_firewall.bat
@@ -1,4 +1,4 @@
 @echo off
-netsh advfirewall firewall delete rule name="Sync-ODDR"
+netsh advfirewall firewall delete rule name=all dir=in protocol=tcp localport=10800,10801
 netsh advfirewall firewall add rule name="Sync-ODDR" protocol=TCP dir=in localport=10800,10801 action=allow
 pause
